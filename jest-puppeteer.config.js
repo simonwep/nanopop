637f303d0ee728655de68e17fb05d3189b9127c7
Simon Reinisch <simon@reinisch.io>
2020-05-09 16:45:35 +0200

    Fix puppeteer test suite

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,12 @@
+const CI = process.env.CI === 'true';
+
 module.exports = {
+    browser: 'chromium',
     launch: {
         dumpio: true,
         headless: true,
         defaultViewport: null,
+        executablePath: CI ? '/usr/bin/chromium-browser' : undefined,
         args: [`--window-size=${750},${750}`]
     },
     server: {
