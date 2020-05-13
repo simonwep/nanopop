f66980b8c762c4ec2df9a8621d00d4d53dd0a51d
Simon Reinisch <simon@reinisch.io>
2020-05-13 17:08:08 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+import {NanoPop} from 'https://cdn.jsdelivr.net/npm/nanopop/lib/nanopop.min.mjs';
+
+const reference = document.querySelector('.reference');
+const popper = document.querySelector('.popper');
+
+const nanoPop = new NanoPop(reference, popper);
+
+const posControls = Array.from(document.querySelectorAll('.controls .pos > button'));
+const varControls = Array.from(document.querySelectorAll('.controls .var > button'));
+const poss = ['top', 'right', 'bottom', 'left'];
+const vars = ['start', 'middle', 'end'];
+
+let p = 2, v = 1;
+const update = () => {
+    posControls.forEach((el, i) => el.classList[i === p ? 'add' : 'remove']('active'));
+    varControls.forEach((el, i) => el.classList[i === v ? 'add' : 'remove']('active'));
+    nanoPop.update({position: `${poss[p]}-${vars[v]}`});
+};
+
+for (let i = 0; i < posControls.length; i++) {
+    posControls[i].addEventListener('click', () => (p = i, update()));
+}
+
+for (let i = 0; i < varControls.length; i++) {
+    varControls[i].addEventListener('click', () => (v = i, update()));
+}
+
+update();
+
+window.addEventListener('resize', update);
